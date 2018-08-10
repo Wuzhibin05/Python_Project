8b684d284b828a121c6576745a07509423658735
Administrator <wuzhibin05@163.com>
2018-08-10 22:35:13 +0800

    update file

---
+++ python14/day1/MyCode/For.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# __author__ = "Wuzhibin"
+# Email: wuzhibin05@163.com
+# Date: 2018/8/10
+
+# 循环1
+# for i  in range(10):
+#     print("loop",i)
+
+# 循环2
+# age_of_oldboy = 56
+# count = 0
+# for i in range(3):
+#     guess_age = int(input("guess age:"))
+#     if guess_age == age_of_oldboy:
+#         print("yes, you got it !")
+#         break
+#     elif guess_age > age_of_oldboy:
+#         print("Think smaller!")
+#     else:
+#         print("Think bigger!")
+#     count += 1
+# else:
+#    print("You have tried too manay times!")
+
+# 循环3
+for i  in range(0,10,2):
+    print("loop",i)
