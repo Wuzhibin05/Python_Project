7959304022706f5aa4bf06e478ca88fe2c501aa2
Administrator <wuzhibin05@163.com>
2018-08-13 22:39:14 +0800

    update file

---
+++ python14/__init__.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# __author__ = "Wuzhibin"
+# Email: wuzhibin05@163.com
+# Date: 2018/8/13
---
+++ python14/day2/MyCode/sys_demo.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# __author__ = "Wuzhibin"
+# Email: wuzhibin05@163.com
+# Date: 2018/8/13
+
+
+
+
+import sys
+
+# 打印环境变量
+print(sys.path)
+
+# print(sys.argv)
+# print(sys.argv[2])
+
+# 告诉解释器去哪里寻找模块
+sys.path.append('d:/python')
+
